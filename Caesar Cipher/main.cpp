45b4a959d547e2d9d613c98b79073bcca070c2a8
tunkatunk <jasa4958@colorado.edu>
2018-03-26 11:35:56 -0600

    First version

--- Caesar Cipher/main.cpp
+++ Caesar Cipher/main.cpp
@@ -7,9 +7,66 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void encrypt_decrypt(int key, string f_in, string f_out, string state)
+{
+    string line;
+    ifstream file;
+    ofstream encrypt_file;
+    file.open (f_in);
+    encrypt_file.open (f_out);
+    if (file.is_open() and encrypt_file.is_open())
+    {
+        while ( getline (file, line))
+        {
+            string newline;
+            for (char& c : line)
+            {
+                if (c != ' ')
+                {
+                    if (state == "encrypt")
+                    {
+                        int enc = (int) c + key;
+                        char enc_chr = (char) enc;
+                        newline += enc_chr;
+                    }
+                    else if (state == "decrypt")
+                    {
+                        int enc = (int) c - key;
+                        char enc_chr = (char) enc;
+                        newline += enc_chr;
+                    }
+                    else cout << "Error: Invalid State" << endl;
+
+                }
+                else if (c == ' ')
+                {
+                    newline += ' ';
+                }
+            }
+            newline += '\n';
+            encrypt_file << newline;
+        }
+        file.close();
+        encrypt_file.close();
+    }
+    
+    else cout << "Unable to open file" << endl;
+}
+
+int main(void)
+{
+    int key = 75;
+    string f_in = "void.txt";
+    string f_enc = "secret.txt";
+    string enc = "encrypt";
+    string dec = "decrypt";
+    encrypt_decrypt(key, f_in, f_enc, enc);
+    string f_denc = "revealed.txt";
+    encrypt_decrypt(key, f_enc, f_denc, dec);
     return 0;
 }
